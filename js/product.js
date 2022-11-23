6dd2b2039fb1a6d487d9b704afa9c0d840858e5a
marthebull <marthebullpettersen@gmail.com>
2022-11-23 11:37:11 +0100

    can not bid and asked to log in to place bid at sell

---
+++ js/product.js
@@ -0,0 +1,6 @@
+const loginToBidBtn = document.getElementById("login-to-bid");
+
+loginToBidBtn.addEventListener("click", function (e) {
+  window.location.href = "../index.html";
+  console.log("Klikket på knapp");
+});
